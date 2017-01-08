bbf9cd950d562173f067db28bcdd015c3743cb2f
罄天 <qinliang.ql@alibaba-inc.com>
2017-01-08 21:07:22 +0800

    c

--- example/sync.js
+++ example/sync.js
@@ -1,8 +1,14 @@
 var resolve = require('../');
 // console.log('__dirname',__dirname);
 //打印结果为：__dirname /Users/qingtian/Desktop/node-resolve/example
-var res = resolve.sync('tap', { basedir: __dirname });
-console.log(res);
+
+var res = resolve.sync('tap', { basedir: process.cwd() });
+console.log('basedir表示此时从这里开始查找node_modules',res);
+
+
+
+// var res = resolve.sync('tap', { basedir: __dirname });
+// console.log(res);
 //打印：/Users/qingtian/Desktop/node-resolve/node_modules/tap/lib/main.js
 //我们的resolve路径是采用node_modules逐层往上查找的
 // var notNode_Modules=resolve.sync('qinliang.js',{basedir:__dirname});
@@ -22,6 +28,6 @@ console.log(res);
 //因为这里是相对路径，所以他不会再node_modules中查找，而是会直接查找相应的文件
 
 // console.log('cwd---',process.cwd());
-var notNode_Modules=resolve.sync('./../qinliang.js',{basedir:process.cwd()});
-console.log('不在node_modules中的模块',notNode_Modules);
+// var notNode_Modules=resolve.sync('./../qinliang.js',{basedir:process.cwd()});
+// console.log('不在node_modules中的模块',notNode_Modules);
 //process.cwd表示的是运行node的文件夹，而dirname表示存放js的文件路径
